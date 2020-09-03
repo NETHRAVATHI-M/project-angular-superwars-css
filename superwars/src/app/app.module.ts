edfff85f52e84b7a905db8c65f54c673f6d87bd2
NETHRAVATHI-M <nethravathim72@gmail.com>
2020-09-03 17:45:39 +0530

    done

---
+++ superwars/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { HeadComponent } from './head/head.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BodyComponent,
+    HeadComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
